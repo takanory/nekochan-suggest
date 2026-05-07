3e351fea182d64dbded216010df92bec5203204b
Takanori Suzuki <takanori@takanory.net>
2026-05-07 23:24:02 +0900

    chore: change default LLM model to gemma4:e4b

--- nekochan_suggest/query.py
+++ nekochan_suggest/query.py
@@ -21,7 +21,7 @@ ANNOTATIONS_PATH = (
 )
 CONFIG_PATH = Path.home() / ".config" / "nekochan-suggest" / "config.toml"
 DEFAULT_EMBED_MODEL = "intfloat/multilingual-e5-base"
-DEFAULT_LLM_MODEL = "qwen3.5:2b"
+DEFAULT_LLM_MODEL = "gemma4:e4b"
 
 
 @dataclass
